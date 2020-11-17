65db4c508d273f3b5abceda9402cca785096dacc
Ponalbus <59609727+Ponalbus@users.noreply.github.com>
2020-11-17 21:56:45 +0300

    Add files via upload

---
+++ CSS/main.js
@@ -0,0 +1,69 @@
+$('.carusel-Box-common').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+      
+$('.carusel-Boxs2').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+            
+$('.card_Product2').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true
+});
+        
+
+          document.getElementById('mask').onclick = function() {
+      		document.getElementById('banner_rain').style.display = 'none';
+   			 }
+
+   			 document.getElementById('Love_box').onclick = function() {
+      		 document.getElementById('whithout_Love').style.display = 'none';
+      		 document.getElementById('Love_in').style.display = 'block';
+   			 }
+
+   			 document.getElementById('Love_box2').onclick = function() {
+      		 document.getElementById('whithout_Love2').style.display = 'none';
+      		 document.getElementById('Love_in2').style.display = 'block';
+   			 }
+   			  document.getElementById('Love_box3').onclick = function() {
+      		 document.getElementById('whithout_Love3').style.display = 'none';
+      		 document.getElementById('Love_in3').style.display = 'block';
+   			 }
+
+   			 document.getElementById('Love_box4').onclick = function() {
+      		 document.getElementById('whithout_Love4').style.display = 'none';
+      		 document.getElementById('Love_in4').style.display = 'block';
+   			 }
+   			 document.getElementById('Love_box5').onclick = function() {
+      		 document.getElementById('whithout_Love5').style.display = 'none';
+      		 document.getElementById('Love_in5').style.display = 'block';
+   			 }
+   			 document.getElementById('Love_box6').onclick = function() {
+      		 document.getElementById('whithout_Love6').style.display = 'none';
+      		 document.getElementById('Love_in6').style.display = 'block';
+   			 }
+   			 document.getElementById('Love_box7').onclick = function() {
+      		 document.getElementById('whithout_Love7').style.display = 'none';
+      		 document.getElementById('Love_in7').style.display = 'block';
+   			 }
+   			 document.getElementById('Love_box8').onclick = function() {
+      		 document.getElementById('whithout_Love8').style.display = 'none';
+      		 document.getElementById('Love_in8').style.display = 'block';
+   			 }
+
+       
+
+
+
+		
